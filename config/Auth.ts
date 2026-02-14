ec31fe3b36c3a29b42143d781081284629a8b979
joseph <joseph00631@gmail.com>
2026-02-14 23:38:14 +0330

    fix type error

---
+++ config/Auth.ts
@@ -0,0 +1,62 @@
+import { Login } from "@/API/Auth";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "username", type: "username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null;
+
+        const data = await Login(credentials.username, credentials.password);
+
+        console.log("data", data);
+
+        return {
+          id: data.id,
+          name: data.firstName,
+          username: data.email,
+          accessToken: data.accessToken,
+        };
+      },
+    }),
+  ],
+
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+        };
+        token.accessToken = user.accessToken;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (token.user) {
+        session.user = token.user;
+      }
+
+      if (token.accessToken) {
+        session.accessToken = token.accessToken;
+      }
+
+      return session;
+    },
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
